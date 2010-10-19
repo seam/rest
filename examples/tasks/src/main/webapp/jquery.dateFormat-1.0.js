71805030677c15757422b031ecb8af11d6c4a7b5
Jozef Hartinger <jharting@redhat.com>
2010-10-19 12:02:22 +0200

    Upgraded the dateFormat jQuery plugin.

--- examples/tasks/src/main/webapp/jquery.dateFormat-1.0.js
+++ examples/tasks/src/main/webapp/jquery.dateFormat-1.0.js
@@ -76,15 +76,13 @@
                         month = value.getMonth() + 1;
                         dayOfMonth = value.getDate();
                         time = parseTime(value.toTimeString());
-                    } else if (value.indexOf('T') != -1){
-                    	var datePart = value.split("T")[0];
-                    	var timePart = value.split("T")[1].split("+")[0];
-                    	var dateValues = datePart.split("-");
+                    } else if (value.search(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.?\d{0,3}\+\d{2}:\d{2}/) != -1) { // 2009-04-19T16:11:05+02:00
+                    	var values = value.split(/[T\+-]/);
                     	
-                    	year = dateValues[0];
-                    	month = dateValues[1];
-                    	dayOfMonth = dateValues[2];
-                    	time = parseTime(timePart);
+                    	year = values[0];
+                    	month = values[1];
+                    	dayOfMonth = values[2];
+                    	time = parseTime(values[3].split(".")[0]);
                     } else {
                         var values = value.split(" ");
                         
