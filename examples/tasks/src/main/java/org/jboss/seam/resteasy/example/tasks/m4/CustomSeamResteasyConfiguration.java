be9c2e47745a82476710ee2cc539516b3833795e
Jozef Hartinger <jharting@redhat.com>
2010-10-20 23:51:42 +0200

    Temporary workaround to get the example working on JBoss M4.

---
+++ examples/tasks/src/main/java/org/jboss/seam/resteasy/example/tasks/m4/CustomSeamResteasyConfiguration.java
@@ -0,0 +1,21 @@
+package org.jboss.seam.resteasy.example.tasks.m4;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Specializes;
+import javax.persistence.NoResultException;
+
+import org.jboss.seam.resteasy.configuration.ExceptionMapping;
+import org.jboss.seam.resteasy.configuration.SeamResteasyConfiguration;
+
+@Specializes
+public class CustomSeamResteasyConfiguration extends SeamResteasyConfiguration {
+
+	@PostConstruct
+	public void setup()
+	{
+		addExceptionMapping(new ExceptionMapping(NoResultException.class, 404, "Requested resource with id #{pathParameters['id']} does not exist."));
+		addExceptionMapping(new ExceptionMapping(IllegalArgumentException.class, 400, "Illegal parameter value."));
+		addMediaTypeMapping("xml", "application/xml");
+		addMediaTypeMapping("json", "application/json");
+	}
+}
--- impl/src/main/java/org/jboss/seam/resteasy/util/Interpolator.java
+++ impl/src/main/java/org/jboss/seam/resteasy/util/Interpolator.java
@@ -25,16 +25,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
+//import javax.inject.Inject;
 
-import org.jboss.weld.extensions.el.Expressions;
+//import org.jboss.weld.extensions.el.Expressions;
 
 @ApplicationScoped
 public class Interpolator
 {
    public static final Pattern elPattern = Pattern.compile("(#\\{.*?\\})");
-   @Inject
-   private Expressions el;
+//   @Inject
+//   private Expressions el;
 
    public String interpolate(String input)
    {
@@ -43,7 +43,7 @@ public class Interpolator
 
       while (matcher.find())
       {
-         matcher.appendReplacement(buffer, String.valueOf(el.evaluateValueExpression(matcher.group())));
+//         matcher.appendReplacement(buffer, String.valueOf(el.evaluateValueExpression(matcher.group())));
       }
       matcher.appendTail(buffer);
 
