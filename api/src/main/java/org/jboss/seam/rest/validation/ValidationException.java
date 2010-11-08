3df86c1fe19e3241ab027449e21ba3aeb5f6202a
Jozef Hartinger <jharting@redhat.com>
2010-11-08 15:43:19 +0100

    minor

--- api/src/main/java/org/jboss/seam/rest/validation/ValidationException.java
+++ api/src/main/java/org/jboss/seam/rest/validation/ValidationException.java
@@ -21,13 +21,14 @@
  */
 package org.jboss.seam.rest.validation;
 
+import java.util.Iterator;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
 
 public class ValidationException extends RuntimeException
 {
    private static final long serialVersionUID = -2779809222298578247L;
-   
+
    private Set<ConstraintViolation<Object>> violations;
 
    public ValidationException(Set<ConstraintViolation<Object>> violations)
@@ -38,5 +39,21 @@ public class ValidationException extends RuntimeException
    public Set<ConstraintViolation<Object>> getViolations()
    {
       return violations;
+   }
+
+   @Override
+   public String getMessage()
+   {
+      StringBuilder builder = new StringBuilder("[");
+      for (Iterator<ConstraintViolation<Object>> iterator = violations.iterator(); iterator.hasNext();)
+      {
+         builder.append(iterator.next().getMessage());
+         if (iterator.hasNext())
+         {
+            builder.append(", ");
+         }
+      }
+      builder.append("]");
+      return builder.toString();
    }
 }
