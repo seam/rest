183d371852842a86a16016be16225e1b187b9bce
Jozef Hartinger <jharting@redhat.com>
2010-11-10 15:45:35 +0100

    Make DefaultClientExecutorProducer usable in server environment.

--- impl/src/main/java/org/jboss/seam/rest/client/DefaultClientExecutorProducer.java
+++ impl/src/main/java/org/jboss/seam/rest/client/DefaultClientExecutorProducer.java
@@ -23,10 +23,29 @@ package org.jboss.seam.rest.client;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 import org.jboss.resteasy.client.ClientExecutor;
 import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
 import org.jboss.weld.extensions.bean.defaultbean.DefaultBean;
 
+/**
+ * Produces the default ClientExecutor. It can be overriden by providing 
+ * ClientExecutor Bean.
+ * 
+ * @author <a href="mailto:jharting@redhat.com">Jozef Hartinger</a>
+ *
+ */
 @ApplicationScoped
 public class DefaultClientExecutorProducer
 {
@@ -34,6 +53,16 @@ public class DefaultClientExecutorProducer
    @DefaultBean(value = ClientExecutor.class)
    public ClientExecutor createExecutor()
    {
-      return new ApacheHttpClient4Executor();
+      HttpParams params = new BasicHttpParams();
+      ConnManagerParams.setMaxTotalConnections(params, 200);
+
+      SchemeRegistry schemeRegistry = new SchemeRegistry();
+      schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+      schemeRegistry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
+
+      ClientConnectionManager cm = new ThreadSafeClientConnManager(params, schemeRegistry);
+      HttpClient httpClient = new DefaultHttpClient(cm, params);
+
+      return new ApacheHttpClient4Executor(httpClient);
    }
 }
