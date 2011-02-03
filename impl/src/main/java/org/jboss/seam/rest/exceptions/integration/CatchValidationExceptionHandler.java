317197ef0df5af66189af480044b619f0a604eb4
Jozef Hartinger <jharting@redhat.com>
2011-02-03 16:09:38 +0100

    minor

--- impl/src/main/java/org/jboss/seam/rest/exceptions/integration/CatchValidationExceptionHandler.java
+++ impl/src/main/java/org/jboss/seam/rest/exceptions/integration/CatchValidationExceptionHandler.java
@@ -28,6 +28,7 @@ import javax.ws.rs.core.Response.ResponseBuilder;
 import org.jboss.seam.exception.control.CaughtException;
 import org.jboss.seam.exception.control.Handles;
 import org.jboss.seam.exception.control.HandlesExceptions;
+import org.jboss.seam.exception.control.Precedence;
 import org.jboss.seam.exception.control.TraversalMode;
 import org.jboss.seam.rest.exceptions.RestRequest;
 import org.jboss.seam.rest.exceptions.RestResource;
@@ -47,7 +48,7 @@ public class CatchValidationExceptionHandler
    @Inject
    private ValidationExceptionHandler delegate;
    
-   public void handleValidationException(@Handles(precedence = -100, during = TraversalMode.DEPTH_FIRST) @RestRequest CaughtException<ValidationException> event, @RestResource ResponseBuilder builder)
+   public void handleValidationException(@Handles(precedence = Precedence.BUILT_IN, during = TraversalMode.DEPTH_FIRST) @RestRequest CaughtException<ValidationException> event, @RestResource ResponseBuilder builder)
    {
       delegate.handleValidationException(event.getException(), builder);
       event.handled();
