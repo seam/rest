6344beb82869ae2d03016e9c1c4a5418b525a1da
Jozef Hartinger <jharting@redhat.com>
2011-02-16 18:58:26 +0100

    Another GlassFish workaround

--- impl/src/main/java/org/jboss/seam/rest/util/Interpolator.java
+++ impl/src/main/java/org/jboss/seam/rest/util/Interpolator.java
@@ -25,6 +25,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
 import org.jboss.seam.solder.el.Expressions;
@@ -33,8 +34,16 @@ import org.jboss.seam.solder.el.Expressions;
 public class Interpolator
 {
    public static final Pattern elPattern = Pattern.compile("(#\\{.*?\\})");
-   @Inject
    private Expressions el;
+   
+   /**
+    * Workaround for GlassFish
+    */
+   @Inject
+   public void init(Instance<Expressions> expressions)
+   {
+      el = expressions.get();
+   }
 
    /**
     * Evaluates EL expressions contained in the input String.
