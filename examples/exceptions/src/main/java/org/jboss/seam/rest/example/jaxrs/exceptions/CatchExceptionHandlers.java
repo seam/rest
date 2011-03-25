553a9d046b79e86ece3e4c7b893f2f74fab84276
Jozef Hartinger <jharting@redhat.com>
2011-03-25 20:53:22 +0100

    SEAMREST-38

--- examples/exceptions/src/main/java/org/jboss/seam/rest/example/jaxrs/exceptions/CatchExceptionHandlers.java
+++ examples/exceptions/src/main/java/org/jboss/seam/rest/example/jaxrs/exceptions/CatchExceptionHandlers.java
@@ -17,8 +17,7 @@ import org.jboss.seam.rest.exceptions.RestResource;
 public class CatchExceptionHandlers {
     public void arithmeticExceptionHandler(@Handles CaughtException<ArithmeticException> event,
             @RestResource ResponseBuilder builder) {
-        builder.status(500).entity("Cannot divide by zero. Want to divide by two instead?").type(MediaType.TEXT_PLAIN_TYPE)
-                .build();
+        builder.status(500).entity("Cannot divide by zero. Want to divide by two instead?").type(MediaType.TEXT_PLAIN_TYPE);
         event.handled();
     }
 }
