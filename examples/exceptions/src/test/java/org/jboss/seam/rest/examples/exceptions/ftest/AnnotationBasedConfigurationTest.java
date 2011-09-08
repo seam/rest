8013f8bba349fa8e640b120fc6e145687be006c9
Jozef Hartinger <jharting@redhat.com>
2011-09-08 16:57:44 +0200

    migrated rest-exceptions to use Arquillian

--- examples/exceptions/src/test/java/org/jboss/seam/rest/examples/exceptions/ftest/AnnotationBasedConfigurationTest.java
+++ examples/exceptions/src/test/java/org/jboss/seam/rest/examples/exceptions/ftest/AnnotationBasedConfigurationTest.java
@@ -1,22 +1,22 @@
 package org.jboss.seam.rest.examples.exceptions.ftest;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 public class AnnotationBasedConfigurationTest extends AbstractExceptionHandlingTest {
     @Test
     public void testStatusCode() throws Exception {
-        checkResponse("/exceptions?exception=java.lang.IllegalAccessException", 403, null);
+        checkResponse("api/exceptions?exception=java.lang.IllegalAccessException", 403, null);
     }
 
     @Test
     public void testMessageBody() throws Exception {
-        checkResponse("/exceptions?exception=java.lang.NullPointerException", 500,
+        checkResponse("api/exceptions?exception=java.lang.NullPointerException", 500,
                 "<error><message>NullPointerException was thrown.</message></error>");
     }
 
     @Test
     public void testExceptionMessage() throws Exception {
-        checkResponse("/exceptions?exception=java.lang.Object", 500,
+        checkResponse("api/exceptions?exception=java.lang.Object", 500,
                 "<error><message>java.lang.Object is not an exception</message></error>");
     }
 }
